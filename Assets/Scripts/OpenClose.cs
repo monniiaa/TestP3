8472dbd5aa247cd3200dd5f179b554a3d849cf23
monniiaa <74655535+monniiaa@users.noreply.github.com>
2022-11-30 18:02:44 +0100

    OpenClose scripr for rotating and translating objects

---
+++ Assets/Scripts/OpenClose.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class OpenClose : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject translateObject;
+    private bool _isOpen = false;
+    [SerializeField]
+    private bool rotate;
+
+    bool _inRange;
+
+    [SerializeField]
+    private Vector3 translation;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _inRange = true;
+            //TODO:Activate canvas
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _inRange = false;
+            //TODO:Activate canvas
+        }
+    }
+    void Rotate(Vector3 translation)
+    {
+        translateObject.transform.Rotate(translation);
+    }
+
+    void Translate(Vector3 translation)
+    {
+        translateObject.transform.Translate(translation);
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E) && _inRange)
+        {
+            if (_isOpen && rotate)
+            {
+                _isOpen = false;
+                Rotate(translation);
+            }
+            else if (_isOpen && !rotate)
+            {
+                _isOpen = false;
+                Translate(-translation);
+            }
+            else if (!_isOpen && rotate)
+            {
+                _isOpen = true;
+                Rotate(-translation);
+            }
+            else if (!_isOpen && !rotate)
+            {
+                _isOpen =true;
+                Translate(translation);
+            }
+        }
+    }
+
+}
