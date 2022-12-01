0f74a59091dad444627efd82daa6ba9dafa5bcef
monniiaa <74655535+monniiaa@users.noreply.github.com>
2022-12-01 15:28:57 +0100

    created cutscene animator

--- Assets/Scripts/CutSceneAnimator.cs
+++ Assets/Scripts/CutSceneAnimator.cs
@@ -1,15 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CutSceneAnimator : MonoBehaviour
 {
     [SerializeField]
-    private Camera[] cinematicCameras;
+    private CinemachineVirtualCamera cameraOne;
+    [SerializeField]
+    private CinemachineVirtualCamera cameraTwo;
 
-    private void ChangeCamera(Camera initialCam, Camera endCam)
+    [SerializeField]
+    private int time;
+
+
+
+    private void Start()
+    {
+        cameraOne.gameObject.SetActive(true);
+        StartCoroutine(CameraTransition(time, cameraOne, cameraTwo));
+    }
+
+
+    private void ChangeCamera(CinemachineVirtualCamera initialCam, CinemachineVirtualCamera endCam)
     {
         endCam.gameObject.SetActive(true);
-        initialCam.gameObject.SetActive(false);
+        cameraOne.gameObject.SetActive(false);
     }
+
+
+    IEnumerator CameraTransition(int time,CinemachineVirtualCamera initialcam, CinemachineVirtualCamera endCam)
+    {
+        yield return new WaitForSeconds(time);
+        ChangeCamera(initialcam, endCam);
+    }
+
 }
