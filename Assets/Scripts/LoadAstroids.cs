e6e195da6d1d38c81faee982c86a97f6e3aff4ca
Simsmeister <simon.sand.andersen@hotmail.com>
2022-12-07 10:50:27 +0100

    AsteroidsFadeee

--- Assets/Scripts/LoadAstroids.cs
+++ Assets/Scripts/LoadAstroids.cs
@@ -6,27 +6,31 @@ public class LoadAstroids : MonoBehaviour
 {
     public string sceneName;
     public int time;
+    public int time2;
     public Canvas canvas;
     public bool inGame = false;
     public AudioSource audioSource;
     public AudioClip clip;
+    public GameObject fadeToBlack;
     // Start is called before the first frame update
     void Start()
     {
         
         if (inGame) canvas.gameObject.SetActive(false);
-        StartCoroutine(LoadScene(sceneName, time));
+        StartCoroutine(LoadScene(sceneName, time, time2));
     }
 
 
-    IEnumerator LoadScene(string name, int time)
+    IEnumerator LoadScene(string name, int time, int time2)
     {
         yield return new WaitForSeconds(time);
-        SceneManager.LoadScene(name);
         if (inGame) canvas.gameObject.SetActive(true);
         audioSource.Stop();
         audioSource.clip = clip; 
         audioSource.Play();
+        fadeToBlack.SetActive(true);
+        yield return new WaitForSeconds(time2);
+        SceneManager.LoadScene(name);
 
     }
 }
